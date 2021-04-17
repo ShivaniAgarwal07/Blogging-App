c11ffeca52a67414f783ecc1cb3c1e4462763bf7
Moonwalker <abhisk.mhra@gmail.com>
2021-04-17 20:22:26 +0530

    using props for showing blogs

--- src/Comps/blogList.js
+++ src/Comps/blogList.js
@@ -1,34 +1,10 @@
 import {useState} from 'react';
- const BlogList = () => {
-    const [blogs, setBlogs] = useState([
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:1
-        },
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:1
-        },
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:2
-        },
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:3
-        },
-    ])   
-    
+ const BlogList = (props) => {
+   const bloglist = props.bloglist
+   const title=props.title;
     return ( <>
-    {     blogs.map((blog)=>(
+        {
+      bloglist.map((blog)=>(
         <div 
         
         // EACH element must have a unique key property :- react uses to track each item in DOM by key
@@ -38,7 +14,8 @@ import {useState} from 'react';
                 <p>{blog.body}</p>
                 <h3>Writtern by :{blog.author}</h3>
             </div>
-            ))}
+            ))
+               }
     </> );
  }
   
--- src/Comps/home.js
+++ src/Comps/home.js
@@ -1,11 +1,39 @@
 import BlogList from "./blogList"
-
+import {useState} from 'react';
 
 const Home = () => {
-   
+    const [blogs, setBlogs] = useState([
+        {
+        title:"My New Website", 
+        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
+        author:"Moonwalker",
+        id:1
+        },
+        {
+        title:"My T Website", 
+        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
+        author:"Moonwalker",
+        id:1
+        },
+        {
+        title:"My Sec Website", 
+        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
+        author:"Moonwalker",
+        id:2
+        },
+        {
+        title:"My Latest Website", 
+        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
+        author:"Moonwalker",
+        id:3
+        },
+    ])    
     return ( 
         <div className="home">
-            <BlogList/>
+            <BlogList
+            bloglist={blogs}
+            title="My Latest Website"
+            />
         </div>  
      );
 }
