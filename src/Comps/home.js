a85d945aed05505edf6f640efe5048e102496098
Moonwalker <abhisk.mhra@gmail.com>
2021-04-17 19:08:43 +0530

    useState hook used for state change via event

--- src/Comps/home.js
+++ src/Comps/home.js
@@ -1,8 +1,22 @@
+
+import {useState} from 'react';
+
+
+
 const Home = () => {
+    const [reactive, setReactive] = useState("Abhishek");
+    const changevalue = () => {
+        var name = prompt("Please input your name");
+        setReactive(name);
+    }
     return ( 
         <div className="home">
             <h2>HomePage</h2>
-        </div>
+            <h3>Hello {reactive}</h3>
+            <button onClick={changevalue}>
+                ChangeName
+            </button>
+        </div>  
      );
 }
  
