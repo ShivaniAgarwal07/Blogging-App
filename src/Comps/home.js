07841e48f0da7209af193c084323e1581f5896c9
Moonwalker <abhisk.mhra@gmail.com>
2021-04-17 20:09:59 +0530

    imported blogsList comp

--- src/Comps/home.js
+++ src/Comps/home.js
@@ -1,51 +1,11 @@
-import {useState} from 'react';
+import BlogList from "./blogList"
+
 
 const Home = () => {
    
-    // using array of blogs for our page
-    const [blogs, setBlogs] = useState([
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:1
-        },
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:1
-        },
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:2
-        },
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:3
-        },
-    ])
-
     return ( 
         <div className="home">
-           {/* using map() to iterate through blogs array to view  */}
-           {
-            blogs.map((blog)=>(
-        <div 
-        
-        // EACH element must have a unique key property :- react uses to track each item in DOM by key
-        key={blog.id}
-        className="blogs">
-                <h2>{blog.title}</h2>
-                <p>{blog.body}</p>
-                <h3>Writtern by :{blog.author}</h3>
-            </div>
-            ))
-           }
+            <BlogList/>
         </div>  
      );
 }
