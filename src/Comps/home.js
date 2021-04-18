b11b71095a9ab544220d7ec8ecc8507f63166805
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 11:27:14 +0530

    fetching data from our Local JSON Server using useEffect Hook

--- src/Comps/home.js
+++ src/Comps/home.js
@@ -1,42 +1,23 @@
 import BlogList from "./blogList"
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-        title:"My New Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:1
-        },
-        {
-        title:"My T Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Moonwalker",
-        id:2
-        },
-        {
-        title:"My Sec Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Abhishek",
-        id:3
-        },
-        {
-        title:"My Latest Website", 
-        body:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ducimus voluptatem facilis. Doloremque consequatur ipsa ex tempore corrupti, veritatis nihil!",
-        author:"Abhishek",
-        id:4
-        },
-    ])    
+    const [blogs, setBlogs] = useState(null )    
 
     const handledelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(()=>{
+        fetch("http://localhost:8000/blogs")
+        .then((response) =>{ return response.json()})
+        .then((data) =>setBlogs(data))
+    },[])
+
     return ( 
         <div className="home">
-            <BlogList bloglist={blogs} title="All Blogs!" handleDelete={handledelete}/>
-        
+            {blogs &&  <BlogList bloglist={blogs} title="All Blogs!" handleDelete={handledelete}/>}
         {/* reusing blogs comp with diff filtered data */}
             {/* <BlogList bloglist={blogs.filter((blog) =>blog.author==="Abhishek")} title="Blogs by Abhishek"/> */}
         </div>  
