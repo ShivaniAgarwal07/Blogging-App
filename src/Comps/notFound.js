97e96c2c67e399c36f81ff6639e0bad4eef3b56e
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 19:26:27 +0530

    Not found component completed

--- src/Comps/notFound.js
+++ src/Comps/notFound.js
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+
 const NotFound = () => {
     return ( <div className="notFound">
         <h2>AW Snap ! Looks like you are looking for something that doesn't exist !
