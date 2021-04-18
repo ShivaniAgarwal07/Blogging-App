1b391a518b88655c96da0f49ef2556204563e896
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 14:16:56 +0530

    react router route for / home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import NavBar from './Comps/nav';
 import Home from './Comps/home';
+import {BrowserRouter as Router , Route, Switch } from 'react-router-dom'
 function App() {
   return (
+    <Router>
     <div className='App'>
     <NavBar/>
     <div className="content">
-    <Home/>
+    <Switch>
+    <Route path='/'>
+      <Home/>
+    </Route>
+    </Switch>
     </div>
-
     </div>
+    </Router>
   );
 }
 
