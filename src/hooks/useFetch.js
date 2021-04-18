5b66025adbffc578b65c08c878a1d8fe8e598232
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 13:40:56 +0530

    passing URL Correctly

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 
 import {useState,useEffect} from 'react';
-const useFetch = ({url}) => {
+const useFetch = (url) => {
     const [data, setData] = useState(null );    
     const [error, setError] = useState(null);
     const [isPending, setPending] = useState(true);
