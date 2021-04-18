c0b40997fbe9b6bd5d04547108ccd2e2c8107680
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 17:51:46 +0530

    correctly implemented blogdetails url

--- src/Comps/blogDetails.js
+++ src/Comps/blogDetails.js
@@ -1,22 +1,22 @@
 import {useParams} from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
-import BlogList from "./blogList"
 const BlogDetails = () => {
     
-    // const url = "http://localhost:8000/blogs/details/";
+    const url = "http://localhost:8000/blogs/";
     const {id} = useParams();
-    const {data:blogs,error,isPending} = useFetch("http://localhost:8000/blogs/details/"+id);
+   const {data: blogs,error,isPending}= useFetch(url + id);
+    console.log(blogs);
     return ( 
-        // <div className="blogdetails">
-            <div className="home">
+            <div className="blogdetails">
             {error && <div className="error">{error}</div>}
             {isPending && <div>Loading  . . .</div>}
             {blogs &&  <article>
                 <h2>{blogs.title}</h2>
                 <p>{blogs.body}</p>
-                </article>}
+                <h3>{blogs.author}</h3>
+               </article>}
+
         </div>  
-        // </div>
      );
 }
  
