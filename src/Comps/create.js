96dddf1ed42f3eeec4711e0306c358bbf24cc3c8
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 18:32:20 +0530

    assigned the states of diff input fields to track the change of states in input

--- src/Comps/create.js
+++ src/Comps/create.js
@@ -1,18 +1,33 @@
+import {useState} from 'react';
 const CreateBlog = () => {
+
+    // declaring some states to manage the form input values 
+    const [title, setTitle] =useState("");
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("");
+
     return ( <div className="create">
         <form><h2>Add New Blog</h2>
             <label>Blog Title</label>
-            <input required type="text" name="" id=""/>
+            <input required type="text" 
+            onChange={(event)=>setTitle(event.target.value)}
+            value={title}
+            name="" id=""/>
             <label>Content</label>
-            <textArea required></textArea>
+            <textArea
+            onChange={(event)=>setContent(event.target.value)}
+            value={content}
+            required></textArea>
             <label>Author</label>
-            <select name="" id="">
+            <select
+            onChange={(event)=>setAuthor(event.target.value)}
+            value={author}
+            name="" id="">
                 <option value="Abhishek">Abhishek</option>
                 <option value="Moonwalker">Moonwalker</option>
             </select>
             <button>Add Blog</button>
         </form>
-    
     </div> 
     );
 }
