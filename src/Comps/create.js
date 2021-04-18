11328755d30dd0c5f3e92dec8c9acf825db126f6
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 17:59:34 +0530

    WIP: form for adding new blog

--- src/Comps/create.js
+++ src/Comps/create.js
@@ -1,6 +1,18 @@
 const CreateBlog = () => {
     return ( <div className="create">
         <h2>Add New Blog</h2>
+        <form>
+            <label>Blog Title</label>
+            <input required type="text" name="" id=""/>
+            <label>Content</label>
+            <textArea required></textArea>
+            <label>Author</label>
+            <select name="" id="">
+                <option value="Abhishek">Abhishek</option>
+                <option value="Moonwalker">Moonwalker</option>
+            </select>
+        </form>
+    
     </div> 
     );
 }
