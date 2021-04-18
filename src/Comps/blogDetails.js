703f537a93e87dcd3e75969d1d0c4f814501ce37
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 15:26:55 +0530

    wip: error in displaying blogs : checkpoint

--- src/Comps/blogDetails.js
+++ src/Comps/blogDetails.js
@@ -1,11 +1,22 @@
 import {useParams} from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import BlogList from "./blogList"
 const BlogDetails = () => {
-
+    
+    // const url = "http://localhost:8000/blogs/details/";
     const {id} = useParams();
+    const {data:blogs,error,isPending} = useFetch("http://localhost:8000/blogs/details/"+id);
     return ( 
-        <div className="blogdetails">
-            <h1>Blog Details {id}</h1> 
-        </div>
+        // <div className="blogdetails">
+            <div className="home">
+            {error && <div className="error">{error}</div>}
+            {isPending && <div>Loading  . . .</div>}
+            {blogs &&  <article>
+                <h2>{blogs.title}</h2>
+                <p>{blogs.body}</p>
+                </article>}
+        </div>  
+        // </div>
      );
 }
  
