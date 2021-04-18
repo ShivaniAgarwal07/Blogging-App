31c0d62e97121bd9581f608bba4421066f2aae02
Moonwalker <abhisk.mhra@gmail.com>
2021-04-18 19:22:21 +0530

    Not found component for handling 404 error

---
+++ src/Comps/notFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return ( <div className="notFound">
+        <h2>AW Snap ! Looks like you are looking for something that doesn't exist !
+        </h2>
+        <Link to={"/"}>Back to Home</Link>
+    </div> );
+}
+ 
+export default NotFound;
